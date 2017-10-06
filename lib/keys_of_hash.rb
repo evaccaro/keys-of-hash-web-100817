f22806c84c27830ec0f4dd524c342ccb00040c4c
Elisa Vaccaro <erv230@nyu.edu>
2017-10-06 15:38:03 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,13 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     answer = []
+    arguments.each do |arg|
     self.each do |name, place|
-    if arguments == place
+    if arg == place
       answer.push(name)
     end
   end
+end
       answer
   end
 end
